afe99339f157f324bf0bd0497ee0cfb4dfaea3f1
Adriana <dri19tcc@gmail.com>
2016-06-29 21:14:53 -0700

    Added a class and removed it shortly thereafter to make it look like it's depressed in the button

--- noise.js
+++ noise.js
@@ -13,8 +13,6 @@ $(document).ready( function() {
   })
 
   $('.note.d').click(function(){
-    console.log(event);
-
     d.currentTime = 0;
     d.play();
   })
@@ -47,26 +45,54 @@ $(document).ready( function() {
 // keynotes
   $(document).keypress(function(){ // instead of .cl.ick can also use .mousedown.  Use .mouseover for "strumming"
     if (event.which === 97) {
+      $('.note.c').addClass("active")
       c.currentTime = 0;
       c.play();
+      setTimeout(function() {
+        $('.note.c').removeClass("active")
+      }, 200)
     } else if (event.which === 115) {
+      $('.note.d').addClass("active")
       d.currentTime = 0;
       d.play();
+      setTimeout(function() {
+        $('.note.d').removeClass("active")
+      }, 200)
     } else if (event.which === 100) {
+      $('.note.e').addClass("active")
       e.currentTime = 0;
       e.play();
+      setTimeout(function() {
+        $('.note.e').removeClass("active")
+      }, 200)
     } else if (event.which === 102) {
+      $('.note.f').addClass("active")
       f.currentTime = 0;
       f.play();
+      setTimeout(function() {
+        $('.note.f').removeClass("active")
+      }, 200)
     } else if (event.which === 103) {
+      $('.note.g').addClass("active")
       g.currentTime = 0;
       g.play();
+      setTimeout(function() {
+        $('.note.g').removeClass("active")
+      }, 200)
     } else if (event.which === 104) {
+      $('.note.a').addClass("active")
       a.currentTime = 0;
       a.play();
+      setTimeout(function() {
+        $('.note.a').removeClass("active")
+      }, 200)
     } else if (event.which === 106) {
+      $('.note.b').addClass("active")
       b.currentTime = 0;
       b.play();
+      setTimeout(function() {
+        $('.note.b').removeClass("active")
+      }, 200)
     }
   })
 });
